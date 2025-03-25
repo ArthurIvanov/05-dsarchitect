08b26ce4ebcfada3a47edaa0418f2b47bb5de9a3
Artur Ivanov <artur.dsgn@yandex.ru>
2025-03-25 23:10:52 +0300

    added text-input component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,3 +7,6 @@ export * from "./card";
 export * from "./icon";
 export * from "./placeholder";
 export * from "./spinner";
+export * from "./inline-message";
+export * from "./label";
+export * from "./input-text";
